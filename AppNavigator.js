8238ec5f9f37bacb882ef940886f2eacbfb08c97
akhan2000 <ak556@duke.edu>
2023-09-26 00:27:55 -0400

    Implement AddEventScreen
    - Initialize React Native project with necessary configurations
    - Add navigation structure using React Navigation
    - Create AddEventScreen with form fields for adding new events
      - Implement TextInput for event name, location, and description
      - Add DateTimePicker for event timing
      - Include a Menu for food type selection
    - Add basic validation and HelperTexts for form fields
    - Integrate React Native Paper for UI components

--- AppNavigator.js
+++ AppNavigator.js
@@ -4,16 +4,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MapScreen from './screens/MapScreen';
 import AddEventScreen from './screens/AddEventScreen';
 import UserPreferencesScreen from './screens/UserPreferencesScreen';
+import AddEventScreen from './screens/AddEventScreen'; 
+import HomeScreen from './screens/HomeScreen';
+
 
 const Stack = createStackNavigator();
+
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Map">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
         <Stack.Screen name="AddEvent" component={AddEventScreen} />
         <Stack.Screen name="UserPreferences" component={UserPreferencesScreen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/AddEventScreen.js
+++ screens/AddEventScreen.js
@@ -1,12 +1,129 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { TextInput, Button, HelperText, Menu, Divider, Text } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useNavigation } from '@react-navigation/native';
 
-const AddEventScreen = () => {
+export default function AddEventScreen() {
+  const [eventName, setEventName] = useState('');
+  const [eventLocation, setEventLocation] = useState('');
+  const [eventTime, setEventTime] = useState('');
+  const [foodType, setFoodType] = useState('');
+  const [eventDescription, setEventDescription] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);      
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShow(true);
+  };
+
+  const isFormValid = () => {
+    return eventName && eventLocation && eventTime && foodType && eventDescription;
+  };
+
+  const addEvent = () => {
+    if (isFormValid()) {
+      // Add event to database
+      console.log('Event added');
+      // Navigate back to home screen
+      const navigation = useNavigation();
+      navigation.navigate('Home');
+    }
+  };
+
   return (
-    <View>
-      <Text>This is the Add Event Screen</Text>
-    </View>
-  );
-};
+    <ScrollView style={styles.container}>
+      <HelperText type="error" visible={!eventName}>
+       Event name is required
+      </HelperText>
+      <TextInput
+        label="Event Name"
+        value={eventName}
+        onChangeText={text => setEventName(text)}
+        style={styles.inputField}
+      />
+      <HelperText type="error" visible={false}> {/* Add validation */}
+        Event name is required
+      </HelperText>
 
-export default AddEventScreen;
+      <TextInput
+        label="Event Location"
+        value={eventLocation}
+        onChangeText={text => setEventLocation(text)}
+        style={styles.inputField}
+      />
+      <HelperText type="error" visible={false}> {/* Add validation */}
+        Location is required
+      </HelperText>
+
+      {/* DateTime Picker */}
+      <View>
+        <Button onPress={showDatepicker} title="Pick a Date and Time" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="datetime"
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+      
+      <Menu
+        visible={menuVisible}
+        onDismiss={() => setMenuVisible(false)}
+        anchor={
+          <Button onPress={() => setMenuVisible(true)}>
+            {foodType || 'Select Food Type'}
+          </Button>
+        }
+      >
+        <Menu.Item onPress={() => { setFoodType('Vegan'); setMenuVisible(false); }} title="Vegan" />
+        <Divider />
+        <Menu.Item onPress={() => { setFoodType('Vegetarian'); setMenuVisible(false); }} title="Vegetarian" />
+        <Divider />
+        <Menu.Item onPress={() => { setFoodType('Non-Vegetarian'); setMenuVisible(false); }} title="Non-Vegetarian" />
+      </Menu>
+      
+      <TextInput
+        label="Event Description"
+        value={eventDescription}
+        onChangeText={text => setEventDescription(text)}
+        multiline={true}
+        numberOfLines={4}
+        style={styles.inputField}
+      />
+
+      <Button mode="contained" onPress={addEvent} style={styles.addButton}>
+        Add Event
+      </Button>
+      <Button mode="outlined" onPress={() => {}} style={styles.cancelButton}>
+        Cancel
+      </Button>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  inputField: {
+    marginBottom: 16,
+  },
+  addButton: {
+    marginVertical: 16,
+  },
+  cancelButton: {
+    marginVertical: 16,
+  },
+});
